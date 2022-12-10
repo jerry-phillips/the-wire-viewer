6e6d121e831a72505610ca440b10666d129ade7a
jerseid-BU <93230095+jerseid-BU@users.noreply.github.com>
2022-12-10 16:04:32 -0500

    update basetest

--- app/src/test/java/com/sample/wireviewer/characterlist/BaseTest.kt
+++ app/src/test/java/com/sample/wireviewer/characterlist/BaseTest.kt
@@ -1,18 +1,13 @@
 package com.sample.wireviewer.characterlist
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
-import org.junit.Rule
 
 abstract class BaseTest {
-    @Rule
-    @JvmField
-    val instantExecutorRule = InstantTaskExecutorRule()
 
     private val mainThreadSurrogate = newSingleThreadContext("UI thread")
 
